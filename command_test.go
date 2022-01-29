68e85b1bd2ae6c3eba0b3d19149aff1d89414011
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-01-29 14:48:18 +0900

    extract list_entries tests to other package

--- command_test.go
+++ command_test.go
@@ -13,56 +13,12 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"toggl_time_entry_manipulator/command"
-	"toggl_time_entry_manipulator/command/list"
 	"toggl_time_entry_manipulator/command/get"
 	"toggl_time_entry_manipulator/command/stop"
 	"toggl_time_entry_manipulator/command/modify"
 	"toggl_time_entry_manipulator/domain"
 	"toggl_time_entry_manipulator/repository"
 )
-
-type ListEntryTestSuite struct {
-    suite.Suite
-    mockedRepo *repository.MockedCachedRepository
-    com *list.ListEntryCommand
-}
-
-func TestListEntryTestSuite(t *testing.T) {
-    suite.Run(t, new(ListEntryTestSuite))
-}
-
-func (suite *ListEntryTestSuite) SetupTest() {
-    suite.mockedRepo = &repository.MockedCachedRepository{}
-    suite.com = &list.ListEntryCommand{
-        Repo: suite.mockedRepo,
-    }
-}
-
-func (suite *ListEntryTestSuite) TestItems() {
-    // given
-    arg := "2"
-    dataStr := ""
-    suite.mockedRepo.On("Fetch").Return([]domain.TimeEntryEntity{
-        {Entry: toggl.TimeEntry{ID: 1, Description: "item1"}},
-        {Entry: toggl.TimeEntry{ID: 2, Description: "item2-1"}},
-        {Entry: toggl.TimeEntry{ID: 3, Description: "item2-2"}},
-    }, nil).Once()
-    suite.mockedRepo.On("GetProjects").Return([]toggl.Project{}, nil)
-
-    // when
-    items, _ := suite.com.Items(arg, dataStr)
-
-    // then
-    t := suite.T()
-    assert.Equal(t, 2, len(items))
-    item := items[0]
-    assert.Equal(t, "item2-1 (-)", item.Title)
-    assert.Equal(t, "actual duration: 0 [min], estimation: -", item.Subtitle)
-    var itemData command.DetailRefData
-    err := json.Unmarshal([]byte(item.Arg.Data), &itemData)
-    assert.Nil(t, err)
-    assert.Equal(t, 2, itemData.ID)
-}
 
 type GetEntryTestSuite struct {
     suite.Suite
