fb4a1d21b49cb4b8acf724878068f172cde00bb8
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-01-29 15:45:22 +0900

    extract stop_entry tests to other package

--- command_test.go
+++ command_test.go
@@ -13,50 +13,10 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"toggl_time_entry_manipulator/command"
-	"toggl_time_entry_manipulator/command/stop"
 	"toggl_time_entry_manipulator/command/modify"
 	"toggl_time_entry_manipulator/domain"
 	"toggl_time_entry_manipulator/repository"
 )
-
-type StopEntryTestSuite struct {
-    suite.Suite
-    mockedRepo *repository.MockedCachedRepository
-    com *stop.StopEntryCommand
-}
-
-func TestStopEntryTestSuite(t *testing.T) {
-    suite.Run(t, new(StopEntryTestSuite))
-}
-
-func (suite *StopEntryTestSuite) SetupTest() {
-    suite.mockedRepo = &repository.MockedCachedRepository{}
-    suite.com = &stop.StopEntryCommand{
-        Repo: suite.mockedRepo,
-    }
-}
-
-func (suite *StopEntryTestSuite) TestDo() {
-    // given
-    // given
-    data := command.DetailRefData{ID: 42}
-    dataBytes, _ := json.Marshal(data)
-    dataStr := string(dataBytes)
-    start := time.Now().Add(-time.Hour)
-    runningEntity := domain.TimeEntryEntity{
-        Entry: toggl.TimeEntry{ID: 42, Description: "item42", Start: &start},
-    }
-    suite.mockedRepo.On("FindOneById", 42).Return(runningEntity, nil).Once()
-    suite.mockedRepo.On("Stop", &runningEntity).Return(nil).Once()
-
-    // when
-    out, _ := suite.com.Do(dataStr)
-
-    // then
-    t := suite.T()
-    assert.Equal(t, "Entry has stopped. Description: item42", out)     // TODO WIP
-}
-
 type ModifyEntryTestSuite struct {
     suite.Suite
     mockedRepo *repository.MockedCachedRepository
